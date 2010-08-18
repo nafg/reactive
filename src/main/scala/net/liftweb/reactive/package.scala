5e805116f0348c7f399a47e22833b225580df381
Naftoli Gugenheim <naftoligug@gmail.com>
2010-08-17 22:00:08 -0400

    Added package object. So far contains only 'confirm' method which triggers a confirm box in the browser.

---
+++ src/main/scala/net/liftweb/reactive/package.scala
@@ -0,0 +1,24 @@
+package net.liftweb
+
+import net.liftweb.http.{js, SHtml, S}
+  import js.JE.{JsRaw, Str}
+  import js.JsCmds.Run
+
+package object reactive {
+  def confirm(message: String)(response: PartialFunction[Boolean, Unit])(implicit page: Page): Unit = {
+    val funcWrapper: String=>Unit = {
+      case "true" => if(response.isDefinedAt(true)) response(true)
+      case "false" => if(response.isDefinedAt(false)) response(false)
+    }
+    val js = S.fmapFunc(S.contextFuncBuilder(RElem.ajaxFunc(funcWrapper))) {funcId =>
+      SHtml.makeAjaxCall(
+        JsRaw("'"+funcId+"='+confirm(" + Str(message).toJsCmd + ")")
+      ).toJsCmd
+    }
+    Reactions.inAnyScope(page)(
+      Reactions.queue(
+        Run(js)
+      )
+    )
+  }
+}
