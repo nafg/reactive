7daaf4ffe5295251cc1e890216c1198c52cba46f
Naftoli Gugenheim <naftoligug@gmail.com>
2011-01-05 18:26:01 -0500

    update project configuration

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -10,14 +10,18 @@ class LiftReactive(info: ProjectInfo) extends DefaultProject(info) {
   val scalatools_release = "Scala Tools Release" at
   "http://scala-tools.org/repo-releases/"
 
-  val liftVersion = "2.1-SNAPSHOT"
+  val liftVersion = "2.3-SNAPSHOT"
 
   override def libraryDependencies = Set(
-    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->master",
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-actor" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-util" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-json" % liftVersion % "compile->default",
+    "joda-time" % "joda-time" % "1.6",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     //"nafg" %% "reactive" % "0.0.1",
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default"
+    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
   ) ++ super.libraryDependencies
-
 }
+
