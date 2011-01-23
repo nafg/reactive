c9c66ab967687e8ebed72da6fcee96a5d81dce0f
Naftoli Gugenheim <naftoligug@gmail.com>
2011-01-23 00:38:21 -0500

    web: Refactor Cell out of Span

---
+++ reactive-web/src/main/scala/reactive/web/Cell.scala
@@ -0,0 +1,19 @@
+import scala.xml.NodeSeq
+
+import net.liftweb.http.js.JsCmds.SetHtml
+
+
+trait Cell extends RElem {
+  def content: Signal[NodeSeq]
+  
+  override def render = super.render.copy(child = content.now)
+  
+  override def addPage(implicit page: Page) {
+    super.addPage(page)
+    content foreach {s =>
+      Reactions.inAnyScope(page) {
+        Reactions.queue(SetHtml(id, s))
+      }
+    }
+  }
+}
--- reactive-web/src/main/scala/reactive/web/Span.scala
+++ reactive-web/src/main/scala/reactive/web/Span.scala
@@ -3,28 +3,27 @@ package web
 
 import scala.xml.NodeSeq
 
-import net.liftweb.http.js.JsCmds.SetHtml
 
-
-class Span(content: Signal[NodeSeq] = Val(NodeSeq.Empty)) extends RElem {
-  def baseElem = <span>{content.now}</span>
+trait Span extends RElem {
+  def baseElem = <span/>
   def events = Nil
   def properties = Nil
-  override def addPage(implicit page: Page) {
-    super.addPage(page)
-    content foreach {s =>
-      Reactions.inAnyScope(page) {
-        Reactions.queue(SetHtml(id, s))
-      }
-    }
-  }
+  
   override def toString = "Span(" + render + ")"
 }
 
 object Span {
-  def apply(content: Signal[NodeSeq] = Val(NodeSeq.Empty)) = new Span(content)
+  def apply(content: Signal[NodeSeq] = Val(NodeSeq.Empty)) = {
+    val tmp = content
+    new Span with Cell {
+      def content = tmp
+    }
+  }
   
   def apply(binding: Signal[NodeSeq=>NodeSeq]) = {ns: NodeSeq =>
-    new Span(binding map (_(ns))).render
+    new Span with Cell {
+      lazy val content = binding map (_(ns))
+    }.render
   }  
 }
+
