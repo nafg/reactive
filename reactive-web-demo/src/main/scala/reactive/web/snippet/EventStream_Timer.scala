bd6386961acd30c1ea6eb31b98b7fedf3bb27e96
Naftoli Gugenheim <naftoligug@gmail.com>
2011-02-25 13:07:01 -0500

    web-demo: SimpleDemo, EventStream_Timer: Ref 'this' in until arg
    EventStream_Timer: .5 seconds -> 2 seconds

--- reactive-web-demo/src/main/scala/reactive/web/snippet/EventStream_Timer.scala
+++ reactive-web-demo/src/main/scala/reactive/web/snippet/EventStream_Timer.scala
@@ -4,9 +4,10 @@ import reactive._
 
 
 class EventStream_Timer extends EventStreamDemo {
-  // Create a timer that fires every 1/2 second, starting at 0,
+  // Create a timer that fires every 2 seconds, starting at 0,
   // for 30 seconds
-  val timer = new Timer(0, 500, _ > 30000)
+  // FIXME references this, to keep snippet from being gc'ed
+  val timer = new Timer(0, 2000, {t => this; t > 30000})
   
   override lazy val eventStream = {
     eventSource |
