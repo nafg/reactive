dab9896daf4ad64e3019a4c4eb3183a091a87e0c
Naftoli Gugenheim <naftoligug@gmail.com>
2011-03-01 14:31:24 -0500

    web-demo: Move EventStream demo components from template into Demos object

--- reactive-web-demo/src/main/scala/reactive/web/snippet/EventStream_EventSource.scala
+++ reactive-web-demo/src/main/scala/reactive/web/snippet/EventStream_EventSource.scala
@@ -1,6 +1,6 @@
 package reactive.web.snippet
 
-import scala.xml.NodeSeq
+import scala.xml.{NodeSeq, Text}
 
 import reactive._
   import web._
@@ -25,16 +25,26 @@ class EventStream_EventSource extends EventStreamDemo {
 }
 
 trait EventStreamDemo extends Observing {
-  val text = TextInput()
   lazy val eventSource = new EventSource[String] {}
   lazy val eventStream: EventStream[String] = eventSource
-  lazy val events = SeqSignal(
-    eventStream.foldLeft(List[String]())((list, event) => event :: list).hold(Nil))
-  def render = "input" #> text &
-    "button" #> Button("Fire")(eventSource fire text.value.value.now) &
-    ".div" #> Div {
-      for (elements <- events) yield
-        for (event <- elements) yield
-          RElem(<p>Fired: '{ event }'</p>)
-    }
+  
+  def render = Demos.eventSourceInput(eventSource) ++
+    Demos.eventStreamOutput(eventStream)
 }
+
+object Demos {
+  def eventSourceInput(eventSource: EventSource[String])(implicit o: Observing): NodeSeq = {
+    val text = TextInput()
+    val button = Button("Fire")(eventSource fire text.value.value.now)
+    <xml:group>{text.render} {button.render}</xml:group>
+  }
+  def eventStreamOutput(eventStream: EventStream[String]): NodeSeq = Div {
+    lazy val events = SeqSignal(
+      eventStream.foldLeft(List[String]())((list, event) => event :: list).hold(Nil)
+    )
+    for (elements <- events) yield
+      for (event <- elements) yield
+        RElem(<p>Fired: '{ event }'</p>)
+  }.render
+}
+
