a6f0adc84c57d211e3540ad1b3ffb0599ab7ece6
Naftoli Gugenheim <naftoligug@gmail.com>
2011-03-01 14:49:57 -0500

    web-demo: EventStream_Timer: Do not use EventStreamDemo, union

--- reactive-web-demo/src/main/scala/reactive/web/snippet/EventStream_Timer.scala
+++ reactive-web-demo/src/main/scala/reactive/web/snippet/EventStream_Timer.scala
@@ -3,14 +3,14 @@ package reactive.web.snippet
 import reactive._
 
 
-class EventStream_Timer extends EventStreamDemo {
+class EventStream_Timer extends Observing {
   // Create a timer that fires every 2 seconds, starting at 0,
   // for 30 seconds
   // FIXME references this, to keep snippet from being gc'ed
   val timer = new Timer(0, 2000, {t => this; t > 30000})
   
-  override lazy val eventStream = {
-    eventSource |
-      timer.map(t => "timer: " + t.toString)
-  }
+  def render = Demos.eventStreamOutput(
+    for(t <- timer)
+      yield "timer: " + t.toString
+  )
 }
