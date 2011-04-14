42e545ac3e982be1403565000d99332bcb79a2ca
Naftoli Gugenheim <naftoligug@gmail.com>
2011-04-14 18:19:24 -0400

    Move core and web project definitions into parent Project.scala Switch to Lift 2.4-SNAPSHOT

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 
-class Project(info: ProjectInfo) extends ParentProject(info) {
+trait Common {this: DefaultProject =>
   val mavenLocal = "Local Maven Repository" at
   "file://"+Path.userHome+"/.m2/repository"
 
@@ -11,7 +11,50 @@ class Project(info: ProjectInfo) extends ParentProject(info) {
   val scalatools_release = "Scala-Tools Release" at
   "http://scala-tools.org/repo-releases/"
 
-	lazy val reactive = project("reactive-core")
-	lazy val reactive_web = project("reactive-web", reactive)
+  val sonatype_snapshot = "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
+  
+  val publishTo = "Sonatype Nexus Repository Manager" at "https://oss.sonatype.org/content/repositories/snapshots"
+
+  Credentials(Path fromString "/private/nafg/.credentials", log)
+
+}
+
+
+class Project(info: ProjectInfo) extends ParentProject(info) {
+  lazy val reactive = project(
+    "reactive-core",
+    "reactive-core",
+    new DefaultProject(_) with Common {
+      override def managedStyle = ManagedStyle.Maven
+      override def pomExtra = <xml:group>
+        <name>reactive-core</name>
+        <description>An FRP framework</description>
+        <url>http://wwww.reactive-web.co.cc</url>
+        <licenses></licenses>
+        <scm><url></url><connection></connection></scm>
+        <developers></developers>
+      </xml:group>
+      val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test->default" withSources
+    }
+  )
+  lazy val reactive_web = project(
+    "reactive-web",
+    "reactive-web",
+    new DefaultProject(_) with Common {
+      override def managedStyle = ManagedStyle.Maven
+      override def pomExtra = <xml:group>
+        <name>reactive-web</name>
+        <description>FRP-based abstractions for Ajax and Comet</description>
+        <url>http://wwww.reactive-web.co.cc</url>
+        <licenses></licenses>
+        <scm><url></url><connection></connection></scm>
+        <developers></developers>
+      </xml:group>
+      val liftVersion = "2.4-SNAPSHOT"
+      val testkit = "net.liftweb" %% "lift-testkit" % liftVersion
+      val jetty = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
+      val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test->default" withSources
+    },
+    reactive)
   lazy val reactive_web_demo = project("reactive-web-demo", reactive_web)
 }
