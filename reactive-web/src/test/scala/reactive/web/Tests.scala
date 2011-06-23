468693e68db9bfabad4534be2ec64d9fb9262666
Naftoli Gugenheim <naftoligug@gmail.com>
2011-06-23 16:52:05 -0400

    web: a test

---
+++ reactive-web/src/test/scala/reactive/web/Tests.scala
@@ -0,0 +1,21 @@
+package reactive
+package web
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+import net.liftweb.mockweb._
+
+class RElemTests extends FunSuite with ShouldMatchers {
+}
+
+class DOMPropertyTests extends FunSuite with ShouldMatchers {
+  test("DOMProperty has one id per page") {
+    MockWeb.testS("/") {
+      val property = DOMProperty("someName")
+      val e1 = property.render apply <elem1/>
+      val e2 = property.render apply <elem2/>
+      e1.attributes("id") should equal(e2.attributes("id"))
+    }
+  }
+}
