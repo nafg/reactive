aea9a37ca105b613dc10444f7d4292b49be827ca
Naftoli Gugenheim <naftoligug@gmail.com>
2011-06-24 18:01:15 -0400

    web: Another milestone

--- reactive-web/src/main/scala/reactive/web/javascript/package.scala
+++ reactive-web/src/main/scala/reactive/web/javascript/package.scala
@@ -2,8 +2,15 @@ package reactive
 package web
 
 package object javascript {
-
+  import JsTypes._
+  
   implicit def toJsLiterable[T](x: T)(implicit c: ToJs[T, _]): JsLiterable[T] =
     new JsLiterable[T](x)
-
+    
+  type $[+T<:JsAny] = JsExp[T]
+  
+  type =|>[-P<:JsAny,+R<:JsAny] = JsTypes.JsFunction1[P,R]
+  
+  def $[T<:JsAny] = JsIdent.fresh[T]
+  def $[T<:JsAny](name: Symbol) = JsIdent[T](name)
 }
