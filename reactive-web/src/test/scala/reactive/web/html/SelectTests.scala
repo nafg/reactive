1b82773e80414ad6fdadea8ec85d2a193472cea0
Naftoli Gugenheim <naftoligug@gmail.com>
2011-06-27 01:55:46 -0400

    web: SelectTests: Use MockWeb, check items

--- reactive-web/src/test/scala/reactive/web/html/SelectTests.scala
+++ reactive-web/src/test/scala/reactive/web/html/SelectTests.scala
@@ -5,6 +5,8 @@ package html
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
+import net.liftweb.mockweb._
+
 class SelectTests extends FunSuite with ShouldMatchers with Observing {
   def withNewPage[T](p: => T): T = Page.withPage(new Page)(p)
 
@@ -24,22 +26,26 @@ class SelectTests extends FunSuite with ShouldMatchers with Observing {
   }
 
   test("Replacing items should cause selectedItem to change") {
-    val items = Var(List("A", "B"))
-    val select = withNewPage(Select(items))
+    MockWeb.testS("/") {
+      val items = Var(List("A", "B"))
+      val select = Select(items)
+      select.render
 
-    //    select.selectedItem foreach println
-    //    select.selectedIndex foreach println
+      select.items.now should equal(items.now)
+      select.selectedItem.now should equal(Some("A"))
+      select.selectedIndex.now should equal(Some(0))
 
-    select.selectedItem.now should equal(Some("A"))
-    println("Updating")
+      println("Updating")
+      items() = List("C", "D")
+      select.items.now should equal(items.now)
+      select.selectedItem.now should equal(Some("C"))
+      select.selectedIndex.now should equal(Some(0))
 
-    items() = List("C", "D")
-    select.selectedItem.now should equal(Some("C"))
-    select.selectedIndex.now should equal(Some(0))
-
-    items() = List("E", "F")
-    select.selectedItem.now should equal(Some("E"))
-    select.selectedIndex.now should equal(Some(0))
+      items() = List("E", "F")
+      select.items.now should equal(items.now)
+      select.selectedItem.now should equal(Some("E"))
+      select.selectedIndex.now should equal(Some(0))
+    }
   }
 
 }
