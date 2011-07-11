5a4478dc3d4989579c360f4bda1984978bcc04c7
Naftoli Gugenheim <naftoligug@gmail.com>
2011-07-10 23:29:20 -0400

    web: JsStub enhancement, Window JsStub, JsStatement, make JsRaw a class, function application is a JsStatement

--- reactive-web-demo/src/main/scala/reactive/web/demo/snippet/JsEventStreamDemo.scala
+++ reactive-web-demo/src/main/scala/reactive/web/demo/snippet/JsEventStreamDemo.scala
@@ -9,19 +9,15 @@ import net.liftweb.util._
 import Helpers._
 
 class JsEventStreamDemo extends Observing {
-  trait window extends JsStub {
-    def alert(s: $[JsString]): $[JsVoid]
-  }
-  val window = $$[window]
-
   val clicks = DOMEventSource.click
+  // map all event values to a single string
   val jses = clicks.jsEventStream.map{ (_: $[JsObj]) => "Button clicked"$ }
 
   //this function will be executed in plain javascript, with no ajax involved!
   jses.foreach { v: $[JsString] => window.alert("Fired: ".$ + v) }
 
   //alert from the server too!
-  jses.toServer[String] foreach { v => reactive.web.alert("Server says: '"+v.toString+"'") }
+  jses.toServer[String] foreach { v => window.alert("Server says: '"+v.toString+"'") }
 
   def render =
     "button" #> clicks
--- reactive-web/src/main/scala/reactive/web/javascript/package.scala
+++ reactive-web/src/main/scala/reactive/web/javascript/package.scala
@@ -1,6 +1,8 @@
 package reactive
 package web
 
+import java.lang.reflect.{ InvocationHandler, Proxy }
+
 package object javascript {
   import JsTypes._
 
@@ -15,21 +17,54 @@ package object javascript {
   def $[T <: JsAny] = JsIdent.fresh[T]
   def $[T <: JsAny](name: Symbol) = JsIdent[T](name)
 
-  private[javascript] def classToIdent(c: Class[_]) = c.getName.toList.reverse.dropWhile('$'==).takeWhile('$'!=).reverse.mkString
+  private[javascript] def classToIdent(c: Class[_]) = {
+    val name = c.getSimpleName
+    val lastDollar = name.lastIndexOf('$')
+    val dropEnd = if (name.substring(lastDollar + 1) forall (_.isDigit)) name.length - lastDollar else 0
+    name.toList.reverse.drop(dropEnd).takeWhile('$'!=).reverse.mkString
+  }
 
-  def $$[T <: JsStub : Manifest]: T = {
-    val ih = new java.lang.reflect.InvocationHandler {
-      def invoke(proxy: AnyRef, method: java.lang.reflect.Method, args: Array[AnyRef]): AnyRef = {
-        JsRaw(classToIdent(manifest[T].erasure)+"."+method.getName +
-          (if (args.isEmpty) ""
-          else args.map(_.asInstanceOf[JsExp[_]].render).mkString("(", ",", ")"))
-        )
+  private class StubInvocationHandler[T <: JsStub: Manifest](ident: String) extends InvocationHandler {
+    def invoke(proxy: AnyRef, method: java.lang.reflect.Method, args: Array[AnyRef]): AnyRef = {
+      val clazz: Class[_] = manifest[T].erasure
+      def hasField = try {
+        clazz.getField(method.getName)
+        true
+      } catch {
+        case _: NoSuchFieldException =>
+          false
+      }
+      try { // look for static forwarder
+        val forwarder = Class.
+          forName(clazz.getName+"$class").
+          getMethod(method.getName, clazz +: method.getParameterTypes: _*)
+        forwarder.invoke(null, proxy +: args: _*)
+      } catch {
+        case _: NoSuchMethodException | _: ClassNotFoundException =>
+          new JsRaw(ident+"."+method.getName + {
+            if (args.isEmpty && hasField)
+              ""
+            else if (method.getParameterTypes.forall(classOf[JsExp[_]].isAssignableFrom))
+              args.map(_.asInstanceOf[JsExp[_]].render).mkString("(", ",", ")")
+            else
+              args.map(_.toString).mkString("(", ",", ")")
+          }) with JsStatement {
+            def toReplace = args.collect{ case x: JsExp[_] => x }.toList
+          }
       }
     }
+  }
+  def $$[T <: JsStub: Manifest]: T =
+    JsStub[T](classToIdent(manifest[T].erasure))
+
+  def JsStub[T <: JsStub: Manifest](ident: String): T = {
+    val ih = new StubInvocationHandler(ident)
     java.lang.reflect.Proxy.newProxyInstance(
       getClass.getClassLoader,
       manifest.erasure.getInterfaces :+ manifest.erasure,
       ih
     ).asInstanceOf[T]
   }
+
+  val window = JsStub[Window]("window")
 }
