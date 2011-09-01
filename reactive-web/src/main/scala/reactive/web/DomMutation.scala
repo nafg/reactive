56fdf807319926b7028c45f2ead50bfe2742c970
Naftoli Gugenheim <naftoligug@gmail.com>
2011-09-01 00:51:06 -0400

    web: DomMutation.xformId wasnt working

--- reactive-web/src/main/scala/reactive/web/DomMutation.scala
+++ reactive-web/src/main/scala/reactive/web/DomMutation.scala
@@ -5,7 +5,6 @@ import javascript._
 import scala.xml.Elem
 import scala.xml.NodeSeq
 import scala.xml.Group
-
 import net.liftweb.util.Helpers.encJs
 import net.liftweb.http.js.HtmlFixer
 
@@ -60,10 +59,19 @@ sealed trait DomMutation {
   def parentId: String
   protected def updateElem: Elem => Elem
   def xformId(id: String)(in: NodeSeq)(f: Elem => NodeSeq): NodeSeq = in match {
-    case e: Elem if e.attribute("id").map(_.text) == Some(id) => f(e)
-    case Group(ns) => Group(ns flatMap apply)
-    case Seq(ns@_*) if ns.length > 1 => ns flatMap apply
-    case other => other
+    case e: Elem if e.attribute("id").map(_.text) == Some(id) =>
+      f(e)
+    case Group(ns) =>
+      Group(ns flatMap apply)
+    case s@Seq(ns@_*) =>
+      if (ns.length > 1)
+        ns flatMap apply
+      else if (ns.length == 1 && (s ne ns.head))
+        apply(ns.head)
+      else
+        ns
+    case other =>
+      other
   }
   def apply(in: NodeSeq): NodeSeq = xformId(parentId)(in)(updateElem)
 }
