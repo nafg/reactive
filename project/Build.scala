e1bbbfaaa03fca2d91f47b62ca5492cc54b31772
Naftoli Gugenheim <naftoligug@gmail.com>
2012-01-18 05:18:40 -0500

    Add scalatest full stacktrace option

--- project/Build.scala
+++ project/Build.scala
@@ -30,7 +30,8 @@ object ReactiveBuild extends Build {
     ),
     checksums in update := Nil,
     scalacOptions += "-deprecation",
-    crossScalaVersions := List("2.8.1", "2.9.0-1", "2.9.1")
+    crossScalaVersions := List("2.8.1", "2.9.0-1", "2.9.1"),
+    testOptions in Test += Tests.Argument("-oF")
   )
   val publishingDefaults = defaults ++ Seq(
     publishTo <<= (version) { version: String =>
