555b80458a25761324e4504d70fad7944603eb85
Guillaume Massé <masgui@gmail.com>
2012-08-23 23:33:09 -0400

    CodeInjection snippet render demo with CodeMirror
    not finding html templates
    removed highlight javascript lib
    fixed layout for demo page

--- reactive-web-demo/src/main/scala/reactive/web/demo/snippet/DemoPane.scala
+++ reactive-web-demo/src/main/scala/reactive/web/demo/snippet/DemoPane.scala
@@ -13,24 +13,25 @@ import net.liftweb.common._
 import net.liftweb.sitemap._
   import Loc._
 
+import net.liftweb.doc.snippet.CodeInjection
 
 object DemoPane {
   val menu = Menu.param[String]("ShowDemo","ShowDemo",Full(_),s=>s)  /"showdemo"  >>Hidden
   lazy val loc = menu.toLoc
   
-  def render(xhtml: NodeSeq) = (
-    for {
-      snippetName <- S.attr("snippet") or loc.currentValue
-      layout <- Templates(List("templates-hidden", "demopanelayout"))
-      scalaSource = scala.io.Source.fromInputStream(
-        getClass.getResourceAsStream("/scala-sources/reactive/web/demo/snippet/" + snippetName + ".scala")
-      )
-      template = Templates(List("templates-hidden", snippetName.toLowerCase)) openOr xhtml
-      bind = ".demo [class]" #> ("lift:" + snippetName) &
-        ".demo *" #> template &
-        ".snippet *" #> <pre class="brush: scala">{scalaSource.getLines().mkString("\n")}</pre> &
-        ".template *" #> <pre class="brush: xml">{template.toString}</pre>
-
-    } yield bind(layout)
-  ) openOr NodeSeq.Empty
+  def render(html: NodeSeq) = {
+    (
+      for {
+        snippetName <- S.attr("snippet") or loc.currentValue
+        codePath = ( "/scala-sources/reactive/web/demo/snippet/" + snippetName + ".scala" )
+        htmlPath = ( "/templates-hidden/" + snippetName.toLowerCase )
+        layout <- Templates(List("templates-hidden", "demopanelayout"))
+        template = Templates( htmlPath.split("/").toList ) openOr html
+        bind = ".demo [class]" #> ("lift:" + snippetName) &
+          ".demo *" #> template &
+          ".snippet *" #> CodeInjection.render( codePath ) &
+          ".template *" #> CodeInjection.render( template )
+      } yield bind(layout)
+    ) openOr NodeSeq.Empty
+  }
 }
