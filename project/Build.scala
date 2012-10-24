7301c8123ba048f499672034a673ea2a30e5111e
Naftoli Gugenheim <naftoligug@gmail.com>
2012-10-24 02:05:58 -0400

    2.10.0-{M7->RC1}

--- project/Build.scala
+++ project/Build.scala
@@ -30,12 +30,12 @@ object ReactiveBuild extends Build {
     (scalacOptions in (Compile, doc) <++= (baseDirectory).map{ bd =>
       Seq("-sourcepath", bd.getAbsolutePath, "-doc-source-url", "http://github.com/nafg/reactive/tree€{FILE_PATH}.scala")
     }),
-    crossScalaVersions := List("2.10.0-M7", "2.9.2"),
+    crossScalaVersions := List("2.10.0-RC1", "2.9.2"),
     libraryDependencies <++= (scalaVersion) { v => List(
          "org.scalatest" %% "scalatest" % (
            if(v startsWith "2.8") "1.5"
            else if(v startsWith "2.9") "1.6.1"
-           else "2.0.M4-2.10.0-M7-B1"
+           else "2.0.M4"
          ) % "test" cross CrossVersion.full,
          "org.scalacheck" %% "scalacheck" % (
            if(v startsWith "2.8") "1.8"
