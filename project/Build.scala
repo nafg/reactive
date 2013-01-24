49f1ac0902b7e802f97a586ceb5aeef59d71d624
Naftoli Gugenheim <naftoligug@gmail.com>
2013-01-24 02:30:49 -0500

    Remove scala versions other than 2.10.0

--- project/Build.scala
+++ project/Build.scala
@@ -30,19 +30,12 @@ object ReactiveBuild extends Build {
     (scalacOptions in (Compile, doc) <++= (baseDirectory).map{ bd =>
       Seq("-sourcepath", bd.getAbsolutePath, "-doc-source-url", "http://github.com/nafg/reactive/tree€{FILE_PATH}.scala")
     }),
-    crossScalaVersions := List("2.10.0", "2.9.2"),
-    libraryDependencies <++= (scalaVersion) { v => List(
-         "org.scalatest" %% "scalatest" % (
-           if(v startsWith "2.9") "2.0.M6-SNAP3"
-           else "2.0.M6-SNAP5"
-         ) % "test",
-         "org.scalacheck" %% "scalacheck" % (
-           if(v startsWith "2.8") "1.8"
-           else "1.10.1-SNAPSHOT"
-         ) % "test" cross CrossVersion.full
-      ) ++
-      List("org.scala-lang" % "scala-actors" % v).filter(_ => v startsWith "2.10")
-    },
+    scalaVersion := "2.10.0",
+    libraryDependencies ++= List(
+      "org.scalatest" %% "scalatest" % "2.0.M6-SNAP5" % "test",
+      "org.scalacheck" %% "scalacheck" % "1.10.1-SNAPSHOT" % "test" cross CrossVersion.full,
+      "org.scala-lang" % "scala-actors" % "2.10.0"
+    ),
     testOptions in Test += Tests.Argument("-oF")
   )
   val publishingDefaults = defaults ++ Seq(
