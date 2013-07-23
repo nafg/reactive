e421891fd434a834d0bceefea433357ff852b21f
Naftoli Gugenheim <naftoligug@gmail.com>
2013-07-23 00:48:32 -0400

    web: SelectTests: no need for mockweb

--- reactive-web/src/test/scala/reactive/web/html/SelectTests.scala
+++ reactive-web/src/test/scala/reactive/web/html/SelectTests.scala
@@ -5,11 +5,7 @@ package html
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
-import net.liftweb.mockweb._
-
 class SelectTests extends FunSuite with ShouldMatchers with Observing {
-  implicit val config = Config.defaults
-
   test("Selection should initally be defined") {
     val select = Select(Val(List("A", "B")))
     select.selectedIndex.now.isDefined should be(true)
@@ -17,7 +13,6 @@ class SelectTests extends FunSuite with ShouldMatchers with Observing {
 
   test("Creating an empty Select should not IndexOutOfBounds") {
     Select(Val(Nil))
-    ()
   }
 
   test("When selectedIndex is None then selectedItem is None") {
@@ -27,32 +22,30 @@ class SelectTests extends FunSuite with ShouldMatchers with Observing {
   }
 
   test("Replacing items should cause selectedItem to change") {
-    MockWeb.testS("/") {
-      val items = Var(List("A", "B"))
-      val select = Select(items)
-      implicit val page = new TestPage({ implicit page =>
-        select.render
-      })
+    val items = Var(List("A", "B"))
+    val select = Select(items)
+    implicit val page = new TestPage({ implicit page =>
+      select.render
+    })
 
-      select.items.now should equal(items.now)
-      select.selectedItem.now should equal(Some("A"))
-      select.selectedIndex.now should equal(Some(0))
+    select.items.now should equal(items.now)
+    select.selectedItem.now should equal(Some("A"))
+    select.selectedIndex.now should equal(Some(0))
 
-      items() = List("C", "D")
-      select.items.now should equal(items.now)
-      select.selectedItem.now should equal(Some("C"))
-      select.selectedIndex.now should equal(Some(0))
+    items() = List("C", "D")
+    select.items.now should equal(items.now)
+    select.selectedItem.now should equal(Some("C"))
+    select.selectedIndex.now should equal(Some(0))
 
-      items() = List("E", "F")
-      select.items.now should equal(items.now)
-      select.selectedItem.now should equal(Some("E"))
-      select.selectedIndex.now should equal(Some(0))
+    items() = List("E", "F")
+    select.items.now should equal(items.now)
+    select.selectedItem.now should equal(Some("E"))
+    select.selectedIndex.now should equal(Some(0))
 
-      items() = List("E", "F")
-      select.items.now should equal(items.now)
-      select.selectedItem.now should equal(Some("E"))
-      select.selectedIndex.now should equal(Some(0))
-    }
+    items() = List("E", "F")
+    select.items.now should equal(items.now)
+    select.selectedItem.now should equal(Some("E"))
+    select.selectedIndex.now should equal(Some(0))
   }
 
   test("Replacing items maintains the correct selection") {
