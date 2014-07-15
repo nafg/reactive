19ebbec6388d6b94b4f29b2d5ed784c072197698
Naftoli Gugenheim <naftoligug@gmail.com>
2014-07-14 21:43:42 -0400

    build: small settings refactor, needs sbt 0.13+

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ReactiveBuild extends Build {
   val sonatypeSnapshots = "https://oss.sonatype.org/content/repositories/snapshots/"
   val sonatypeStaging = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
 
-  val defaults = Defaults.defaultSettings ++ Seq(
+  val defaults = Seq(
     organization := "cc.co.scala-reactive",
     resolvers ++= List(
       "Sonatype snapshots" at sonatypeSnapshots,
@@ -24,10 +24,14 @@ object ReactiveBuild extends Build {
     ),
     checksums in update := Nil,
     scalacOptions in (Compile, compile) += "-deprecation",
-    (scalacOptions in (Compile, doc) <++= (baseDirectory).map{ bd =>
-      val sourceUrl = "http://github.com/nafg/reactive/blob/master/" + bd.getName + "€{FILE_PATH}.scala"
-      Seq("-sourcepath", bd.getAbsolutePath, "-doc-source-url", sourceUrl, "-doc-title", "Scaladocs - scala-reactive", "-groups")
-    }),
+    scalacOptions in (Compile, doc) ++= Seq(
+      "-sourcepath",
+      baseDirectory.value.getAbsolutePath,
+      "-doc-source-url",
+      s"http://github.com/nafg/reactive/blob/master/${ baseDirectory.value.getName }€{FILE_PATH}.scala",
+      "-doc-title",
+      "Scaladocs - scala-reactive", "-groups"
+    ),
     libraryDependencies ++= List(
       "org.scalatest" %% "scalatest" % "2.2.0" % "test",
       "org.scalacheck" %% "scalacheck" % "1.11.3" % "test"
@@ -35,68 +39,44 @@ object ReactiveBuild extends Build {
     testOptions in Test += Tests.Argument("-oF")
   )
 
-  val publishingDefaults = defaults ++ Seq(
-    publishTo <<= (version) { version: String =>
-      if (version.trim.endsWith("SNAPSHOT"))
+  val publishingSettings = defaults ++ Seq(
+    publishTo := (
+      if (version.value.trim.endsWith("SNAPSHOT"))
         Some("snapshots" at sonatypeSnapshots)
       else
         Some("staging" at sonatypeStaging)
-    },
+    ),
     publishMavenStyle := true,
     credentials ++= {
       val f = file("/private/nafg/.credentials")
       if(f.exists) Seq(Credentials(f))
       else Nil
-    }
+    },
+    pomExtra := pomCommon
   )
 
-  lazy val reactive_core = Project(
-    "core",
-    file("reactive-core"),
-    settings = publishingDefaults ++ Seq(
-      pomExtra := pomCommon
-    )
-  )
+  val nonPublishingSettings = defaults :+ (publishArtifact := false)
 
-  lazy val reactive_routing = Project(
-    "routing",
-    file("reactive-routing"),
-    settings = publishingDefaults ++ Seq(
-      pomExtra := pomCommon
-    )
-  )
+  lazy val reactive_core = Project("core", file("reactive-core"))
+    .settings(publishingSettings: _*)
 
-  lazy val reactive_web = Project(
-    "web",
-    file("reactive-web"),
-    settings = publishingDefaults ++ Seq(
-      pomExtra := pomCommon
-    )
-  ).dependsOn(reactive_core)
+  lazy val reactive_routing = Project("routing", file("reactive-routing"))
+    .settings(publishingSettings: _*)
 
-  lazy val reactive_web_lift = Project(
-    "web-lift",
-    file("reactive-web-lift"),
-    settings = publishingDefaults ++ Seq(
-      pomExtra := pomCommon
-    )
-  ).dependsOn(reactive_web, reactive_routing)
+  lazy val reactive_web = Project("web", file("reactive-web"))
+    .settings(publishingSettings: _*)
+    .dependsOn(reactive_core)
 
-  lazy val reactive_web_demo = Project(
-    "demo",
-    file("reactive-web-demo"),
-    settings = defaults ++ Seq(
-      publishArtifact := false
-    )
-  ) dependsOn(reactive_web_lift)
+  lazy val reactive_web_lift = Project("web-lift", file("reactive-web-lift"))
+    .settings(publishingSettings: _*)
+    .dependsOn(reactive_web, reactive_routing)
 
-  lazy val root = Project(
-    "scala-reactive",
-    file("."),
-    settings = defaults ++ Seq(
-      publishArtifact := false
-    )
-  )
-  .settings(sbtunidoc.Plugin.unidocSettings: _*)
-  .aggregate(reactive_core, reactive_web, reactive_web_lift, reactive_web_demo, reactive_routing)
+  lazy val reactive_web_demo = Project("demo", file("reactive-web-demo"))
+    .settings(nonPublishingSettings: _*)
+    .dependsOn(reactive_web_lift)
+
+  lazy val root = Project("scala-reactive", file("."))
+    .settings(nonPublishingSettings: _*)
+    .settings(sbtunidoc.Plugin.unidocSettings: _*)
+    .aggregate(reactive_core, reactive_web, reactive_web_lift, reactive_web_demo, reactive_routing)
 }
