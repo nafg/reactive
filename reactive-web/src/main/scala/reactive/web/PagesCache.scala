fa709ed0925a20c021ca3cde39c28a1dffe8c0b0
L Vitaly <lvitalii@gmail.com>
2014-09-07 16:21:19 +0300

    Revert "Fix a ConcurrentModificationException in PagesCache"
    This reverts commit ae0fee3c1c63c9d54f5db27a0f71d9fb58ddd3c0.

--- reactive-web/src/main/scala/reactive/web/PagesCache.scala
+++ reactive-web/src/main/scala/reactive/web/PagesCache.scala
@@ -13,9 +13,7 @@ trait PagesCache {
    */
   def addPage(page: Page): Unit = pagesWeakMap += page -> ()
 
-  private def keys = pagesWeakMap.synchronized { pagesWeakMap.keySet.toSeq }
-
-  protected def getPage(id: String) = keys.toList.find(_.id == id)
+  protected def getPage(id: String) = pagesWeakMap.keys.find(_.id == id)
 
   /**
    * An extractor to locate a `Page` by its id
