7524f84d8f0aa218a3c0a61463f4879e1ecb0bd5
Naftoli Gugenheim <naftoligug@gmail.com>
2017-09-13 13:59:22 -0400

    Switch from upickle to circe (for AjaxCall and AsJs)

--- reactive-web/build.sbt
+++ reactive-web/build.sbt
@@ -4,7 +4,7 @@ description := "FRP-based abstractions to control the browser from the server"
 
 libraryDependencies += "net.liftweb" %% "lift-util" % Dependencies.liftVersion
 
-libraryDependencies += "com.lihaoyi" %% "upickle" % "0.4.4"
+libraryDependencies += "io.circe" %% "circe-parser" % "0.8.0"
 
 libraryDependencies += "org.scalatest"  %% "scalatest"  % "3.0.3"  % "test"
 
--- reactive-web/src/main/scala/reactive/web/JsInterpolator.scala
+++ reactive-web/src/main/scala/reactive/web/JsInterpolator.scala
@@ -1,35 +1,40 @@
 package reactive
 package web
 
-import net.liftweb.json._
-import upickle.Js
-import upickle.default._
+import scala.language.implicitConversions
 
-object LiftJsonToUpickle {
-  def apply(in: JValue): Js.Value = in match {
-    case JString(s)       => Js.Str(s)
-    case JBool(true)      => Js.True
-    case JBool(false)     => Js.False
-    case JInt(i)          => Js.Str(i.toString)
-    case JDouble(d)       => Js.Num(d)
-    case JNull | JNothing => Js.Null
-    case JArray(xs)       => Js.Arr(xs map apply: _*)
-    case JObject(xs)      => Js.Obj(xs map (f => (f.name, apply(f.value))): _*)
+import net.liftweb.json._
+
+import io.circe.syntax.EncoderOps
+import io.circe.{CursorOp, Decoder, Encoder, Json}
+
+
+object LiftJsonToCirce {
+  def apply(in: JValue): Json = in match {
+    case JString(s)       => Json.fromString(s)
+    case JBool(b)         => Json.fromBoolean(b)
+    case JInt(i)          => Json.fromBigInt(i)
+    case JDouble(d)       => Json.fromBigDecimal(d)
+    case JNull | JNothing => Json.Null
+    case JArray(xs)       => Json.arr(xs map apply: _*)
+    case JObject(xs)      => Json.obj(xs map (f => (f.name, apply(f.value))): _*)
   }
   def unapply(in: JValue) = Some(apply(in))
-  implicit def writer: Writer[JValue] = Writer(apply)
 }
-object UpickleToLiftJson {
-  def apply(in: Js.Value): JValue = in match {
-    case Js.Str(s)        => JString(s)
-    case Js.True          => JBool(true)
-    case Js.False         => JBool(false)
-    case Js.Num(d)        => if(d == d.toInt) JInt(d.toInt) else JDouble(d)
-    case Js.Null          => JNull
-    case Js.Arr(xs @ _*)  => JArray(xs.map(apply).toList)
-    case Js.Obj(xs @ _*)  => JObject(xs.map(t => JField(t._1, apply(t._2))).toList)
-  }
-  implicit def reader: Reader[JValue] = Reader{ case x => apply(x) }
+
+object CirceArray {
+  def unapply(json: Json): Option[Seq[Json]] = json.asArray
+}
+
+object CirceToLiftJson {
+  def apply(in: Json): JValue = in.fold(
+    jsonNull = JNull,
+    jsonBoolean = JBool,
+    jsonNumber = n => n.toBigInt.map(JInt).getOrElse(JDouble(n.toDouble)),
+    jsonString = JString,
+    jsonArray = xs => JArray(xs.map(apply).toList),
+    jsonObject = xs => JObject(xs.toList.map(t => JField(t._1, apply(t._2))))
+  )
 }
 
 object AjaxCall {
@@ -38,34 +43,41 @@ object AjaxCall {
     val paramList = StringRenderable(params.map(_.render).mkString(","))
     js"(function($paramList){reactive.queueAjax($id)([$paramList]);reactive.doAjax($pageId);})"
   }
-  private def addHandler(id: String)(pf: PartialFunction[Seq[Js.Value], Unit])(implicit page: Page) = {
+  private def addHandler(id: String)(pf: PartialFunction[Seq[Json], Unit])(implicit page: Page) = {
     import page.observing
     page.ajaxEvents ?>> {
-      case (`id`, LiftJsonToUpickle(Js.Arr(xs @ _*))) if pf isDefinedAt xs =>
+      case (`id`, LiftJsonToCirce(CirceArray(xs))) if pf isDefinedAt xs =>
         pf(xs)
     }
   }
+  private def decode[A: Decoder](json: Json): A =
+    Decoder[A].decodeJson(json) match {
+      case Right(x)      => x
+      case Left(failure) =>
+        throw failure.withMessage("Decoding failure at " + CursorOp.opsToPath(failure.history) + ": " + failure.message)
+    }
+
   def apply(f: () => Unit)(implicit page: Page): StringRenderable = {
     val id = page.nextNumber.toString
-    addHandler(id){ case Seq() => f() }
+    addHandler(id) { case Seq() => f() }
     ajaxFnJs(id, 0, page.id)
   }
 
-  def apply[P1: Reader](f: P1 => Unit)(implicit page: Page) = {
+  def apply[P1: Decoder](f: P1 => Unit)(implicit page: Page) = {
     val id = page.nextNumber.toString
-    addHandler(id){ case Seq(a) => f(readJs[P1](a)) }
+    addHandler(id) { case Seq(a) => f(decode[P1](a)) }
     ajaxFnJs(id, 1, page.id)
   }
 
-  def apply[P1: Reader, P2: Reader](f: (P1, P2) => Unit)(implicit page: Page) = {
+  def apply[P1: Decoder, P2: Decoder](f: (P1, P2) => Unit)(implicit page: Page) = {
     val id = page.nextNumber.toString
-    addHandler(id){ case Seq(a, b) => f(readJs[P1](a), readJs[P2](b)) }
+    addHandler(id) { case Seq(a, b) => f(decode[P1](a), decode[P2](b)) }
     ajaxFnJs(id, 2, page.id)
   }
 
-  def apply[P1: Reader, P2: Reader, P3: Reader](f: (P1, P2, P3) => Unit)(implicit page: Page) = {
+  def apply[P1: Decoder, P2: Decoder, P3: Decoder](f: (P1, P2, P3) => Unit)(implicit page: Page) = {
     val id = page.nextNumber.toString
-    addHandler(id){ case Seq(a, b, c) => f(readJs[P1](a), readJs[P2](b), readJs[P3](c)) }
+    addHandler(id) { case Seq(a, b, c) => f(decode[P1](a), decode[P2](b), decode[P3](c)) }
     ajaxFnJs(id, 3, page.id)
   }
 }
@@ -75,16 +87,18 @@ trait AsJs[A] {
   def render(a: A): String
 }
 object AsJs {
-  def apply[A](f: A => String): AsJs[A] = new AsJs[A] { def render(a: A) = f(a) }
+  def apply[A](f: A => String): AsJs[A] = new AsJs[A] {
+    def render(a: A) = f(a)
+  }
 
   implicit def arity0(implicit page: Page): AsJs[() => Unit] = AsJs(f => AjaxCall(f).string)
-  implicit def arity1[P1: Reader](implicit page: Page): AsJs[P1 => Unit] = AsJs(f => AjaxCall(f).string)
-  implicit def arity2[P1: Reader, P2: Reader](implicit page: Page): AsJs[(P1, P2) => Unit] = AsJs(f => AjaxCall(f).string)
-  implicit def arity3[P1: Reader, P2: Reader, P3: Reader](implicit page: Page): AsJs[(P1, P2, P3) => Unit] = AsJs(f => AjaxCall(f).string)
+  implicit def arity1[P1: Decoder](implicit page: Page): AsJs[P1 => Unit] = AsJs(f => AjaxCall(f).string)
+  implicit def arity2[P1: Decoder, P2: Decoder](implicit page: Page): AsJs[(P1, P2) => Unit] = AsJs(f => AjaxCall(f).string)
+  implicit def arity3[P1: Decoder, P2: Decoder, P3: Decoder](implicit page: Page): AsJs[(P1, P2, P3) => Unit] = AsJs(f => AjaxCall(f).string)
 
   implicit def renderable[R <: Renderable]: AsJs[R] = AsJs(_.render)
 
-  implicit def value[A: Writer]: AsJs[A] = AsJs(a => write(a))
+  implicit def value[A: Encoder]: AsJs[A] = AsJs(_.asJson.noSpaces)
 }
 
 final class JsInterpolatable(string: String) {
